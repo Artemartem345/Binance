420cf041b9d844675f971482fe4c859590c32a07
Artem <Popovartem3412@gmail.com>
2023-07-09 15:26:48 +0300

    first commit

---
+++ Main/auth_data.py
@@ -0,0 +1,4 @@
+auth_username = 'your_username'
+auth_password = 'your_password'
+# КОЛИЧЕСТВО BTS КОТОРОЕ ХОЧЕШЬ ОТПРАВИТЬ
+amount_BTS = 0
---
+++ Main/main.py
@@ -0,0 +1,38 @@
+
+import undetected_chromedriver as uc
+import time
+from auth_data import *
+driver = uc.Chrome()
+driver.get( 'https://accounts.binance.com/ru/login?loginChannel=homepage&return_to=aHR0cHM6Ly93d3cuYmluYW5jZS5jb20vcnUvbXkvZGFzaGJvYXJk')
+username = driver.find_element('id', 'username')
+username.send_keys(auth_username)
+driver.find_element('id','click_login_submit').click()
+time.sleep(10)
+driver.get('https://accounts.binance.com/ru/login-password?isNewLogin=true&loginChannel=homepage&return_to=aHR0cHM6Ly93d3cuYmluYW5jZS5jb20vcnUvbXkvZGFzaGJvYXJk')
+password = driver.find_element('name','password')
+password.send_keys(auth_password)
+driver.find_element('id','click_login_submit').click()
+time.sleep(15)
+driver.get('https://www.binance.com/ru/my/wallet/account/main/withdrawal/crypto/BTS')
+time.sleep(8)
+
+
+adress = driver.find_element('id', 'withdraw-address-selection-input')
+time.sleep(3)
+adress.send_keys('binance-bts-1')
+time.sleep(3)
+
+
+memo_adress = driver.find_element('name', 'memo')
+time.sleep(3)
+memo_adress.send_keys(108802181)
+
+
+amount_withdraw = driver.find_element('id','withdraw-amount-input')
+time.sleep(5)
+amount_withdraw.send_keys(amount_BTS)
+
+
+time.sleep(3)
+withdraw_button = driver.find_element('id','withdraw-submit-btn').click()
+time.sleep(3)
